58b91fa920fc25ee51fab837ba7bed878326468f
RockInMyHead <65908604+RockInMyHead@users.noreply.github.com>
2020-11-02 22:07:59 +0300

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,1 +1,16 @@
-"var b = 2;"
+document.forms.publish.onsubmit = function() {
+    var name = this.name.value;
+    name=name.replace(/ /g,'_');
+    var massage = this.massage.value;
+    massage=massage.replace(/ /g,'_');
+    var email = this.email.value;
+    var url = 'https://sleepy-oasis-49018.herokuapp.com/gmail/' + massage + '/' + name + '/' + email + '/';
+    function goToPage(url)
+        {
+            let script = document.createElement('script');
+            script.src = url;
+            document.body.append(script);
+        }
+    goToPage(url);
+    return false;
+};
