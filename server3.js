96676677cc24a600c0edb6a82f9125d15c6bbe3d
RockInMyHead <65908604+RockInMyHead@users.noreply.github.com>
2023-03-23 07:32:40 +0300

    Create server3.js

---
+++ server3.js
@@ -0,0 +1,17 @@
+document.forms.publish.onsubmit = function() {
+    var name = this.name.value;
+    name=name.replace(/ /g,'_');
+    var massage = this.massage.value;
+    massage=massage.replace(/ /g,'_');
+    var email = this.email.value;
+    var youremail = this.youremail.value;
+    var url = 'http://easyfastmail.ru/users/personal_console/send/' +name + '/' + massage + '/' + email + '/' + youremail ;
+    function goToPage(url)
+        {
+            let script = document.createElement('script');
+            script.src = url;
+            document.body.append(script);
+        }
+    goToPage(url);
+    return false;
+};
